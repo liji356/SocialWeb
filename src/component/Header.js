d6e0f24f846bf28ee56016ed64a436df08eb95a2
liji356 <lijibthomas95@gmail.com>
2022-05-18 13:34:25 +0530

    navbar

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div><nav className="navbar navbar-expand-lg navbar-dark bg-info">
+    <div className="container-fluid">
+      <a className="navbar-brand" href="#">SOCIAL</a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          q
+          <Link className="nav-link active" to={"/"}>Add Post</Link>
+          
+          <Link className="nav-link " to={"/search"}>Search Post</Link>
+          
+          <Link className="nav-link " to={"/view"}>View Post</Link>
+          
+        </div>
+      </div>
+    </div>
+  </nav></div>
   )
 }
 
